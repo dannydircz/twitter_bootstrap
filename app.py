c7ceebf9a1994db11b55efd1fa34dae7cbe457ed
agosto_dannydircz <danny.dircz@agosto.com>
2015-07-21 13:15:08 -0500

    made it better

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 app.config.update(
     DEBUG = True,
 )
 
+
+#controllers
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route('/')
 def hello():
-    return "Hello from Python!"
+    return render_template('index.html')
+
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",5000))
